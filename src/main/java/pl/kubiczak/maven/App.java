c647b66144199c074f599042b4faed98742e034a
Wiiitek <kubiczak.jakub@gmail.com>
2017-12-09 13:52:43 +0100

    initil commit

---
+++ src/main/java/pl/kubiczak/maven/App.java
@@ -0,0 +1,16 @@
+package pl.kubiczak.maven;
+
+/**
+ * Hello world.
+ */
+public class App {
+
+  /**
+   * Main.
+   *
+   * @param args arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/pl/kubiczak/maven/AppTest.java
@@ -0,0 +1,13 @@
+package pl.kubiczak.maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void testApp() {
+    assertTrue(true);
+  }
+}
