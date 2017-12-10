2071ae01f2a268c9db9a7d5dfcca8e98404ee2df
Wiiitek <kubiczak.jakub@gmail.com>
2017-12-10 11:55:49 +0100

    jade test added

--- src/test/java/pl/kubiczak/maven/sling/filters/maven/plugin/jade/JadeTest.java
+++ src/test/java/pl/kubiczak/maven/sling/filters/maven/plugin/jade/JadeTest.java
@@ -3,11 +3,16 @@ package pl.kubiczak.maven.sling.filters.maven.plugin.jade;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import de.neuland.jade4j.exceptions.JadeLexerException;
 import java.io.IOException;
+import org.junit.Rule;
 import org.junit.Test;
-import pl.kubiczak.maven.sling.filters.maven.plugin.jade.Jade;
+import org.junit.rules.ExpectedException;
 
 public class JadeTest {
+
+  @Rule
+  public final ExpectedException exception = ExpectedException.none();
 
   @Test
   public void test() throws IOException {
@@ -20,5 +25,12 @@ public class JadeTest {
         + "</workspaceFilter>";
 
     assertThat(actual, equalTo(expected));
+  }
+
+  @Test
+  public void shouldThrowExceptionForIncorrectSyntax() throws IOException {
+    Jade tested = new Jade();
+    exception.expect(JadeLexerException.class);
+    tested.transformToXml("./incorrect.jade");
   }
 }
