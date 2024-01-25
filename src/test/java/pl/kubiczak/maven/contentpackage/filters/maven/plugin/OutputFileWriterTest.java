c9ebb1e731f2de105076ca2a27b5b2addb437662
Wiiitek <wiiitek@users.noreply.github.com>
2024-01-25 22:00:18 +0100

    Updates for dependencies (#11)
    * Updates for README
    
    * Update for some of the dependencies
    
    * Update for pipeline
    
    * Fix for tests about private files being prefixed on macOS

--- src/test/java/pl/kubiczak/maven/contentpackage/filters/maven/plugin/OutputFileWriterTest.java
+++ src/test/java/pl/kubiczak/maven/contentpackage/filters/maven/plugin/OutputFileWriterTest.java
@@ -1,12 +1,14 @@
 package pl.kubiczak.maven.contentpackage.filters.maven.plugin;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.io.File;
@@ -15,6 +17,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.util.List;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -25,6 +28,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -40,6 +45,9 @@ public class OutputFileWriterTest {
 
   @Mock
   private Log mavenLogMock;
+
+  @Captor
+  ArgumentCaptor<String> errorMessageCaptor;
 
   private File outputFile;
 
@@ -120,12 +128,20 @@ public class OutputFileWriterTest {
     } catch (MojoExecutionException e) {
       // ignore exception in this test
     }
-    verify(mavenLogMock).error(
-        eq("Cannot delete '" + folderPath + "' as it exists but is not a file.")
-    );
-    verify(mavenLogMock).error(
-        eq("File not found or is a folder: '" + folderPath + "'")
-    );
+
+    verify(mavenLogMock, times(2)).error(errorMessageCaptor.capture());
+    List<String> actual = errorMessageCaptor.getAllValues();
+    assertThat(actual.size(), equalTo(2));
+
+    String first = actual.get(0);
+    assertThat(first, startsWith("Cannot delete '"));
+    assertThat(first, containsString(folderPath));
+    assertThat(first, endsWith("' as it exists but is not a file."));
+
+    String second = actual.get(1);
+    assertThat(second, startsWith("File not found or is a folder: '"));
+    assertThat(second, containsString(folderPath));
+    assertThat(second, endsWith("'"));
   }
 
   @Test
